b6e65c9350e862907d2b46e393fde286398b43c2
unknown <you@example.com>
2024-03-16 17:35:26 -0700

    ccpp

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,28 +1,32 @@
-import { promises } from "fs";
-import mongoose,{Mongoose} from "mongoose";
-import { global } from "styled-jsx/css";
-import { connect } from "tls";
-const MONGODB_URL=process.env.MONGODB_URL;
-interface MongooseConnection{
-    conn:Mongoose|null;
-    promise:Promise<Mongoose>|null;
+import mongoose, { Mongoose } from 'mongoose';
 
+const MONGODB_URL = process.env.MONGODB_URL;
+
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
-let cached:MongooseConnection=(global as any).mongoose;
-if(!cached){
-    cached=(global as any).mongoose={
-        conn:null,
-        promise:null
-    }
+
+let cached: MongooseConnection = (global as any).mongoose
+
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
 }
-export const connectToDatabase=async()=>{
-    if(cached.conn)return cached.conn;
-    if(!MONGODB_URL)
-        throw new Error('Missign MONGODB');
-    cached.promise=cached.promise||mongoose.connect(MONGODB_URL,{dbName:'imageinify',bufferCommands:false});
-    cached.conn=await cached.promise;
-    return cached.conn;
-    
 
+export const connectToDatabase = async () => {
+  if(cached.conn) return cached.conn;
 
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'imaginify', bufferCommands: false 
+    })
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
